00e13e8e12982030b68d485349fd8d00fba8f63f
NyeJones <63841568+NyeJones@users.noreply.github.com>
2026-05-23 21:16:24 +0100

    updated index build

--- tei_index_build/render_index.py
+++ tei_index_build/render_index.py
@@ -492,35 +492,37 @@ def render_work(work):
     
     person_ref_names = work.get("person_ref_text")
     person_ref_keys = work.get("person_ref_key")
-    if person_ref_names and person_ref_keys:
+    person_ref_types = work.get("person_ref_type")
+    if person_ref_names and person_ref_keys and person_ref_types:
 
         person_refs = sorted(
-            zip(person_ref_names, person_ref_keys),
+            zip(person_ref_names, person_ref_keys, person_ref_types),
             key=lambda x: normalize_for_sort(x[0])
         )
 
         html.append("<div class='entry-block entry-person-refs'>")
         html.append("<span class='subheading'>Persons Referenced in Work</span>")
-        for name, key in person_refs:
+        for name, key, type in person_refs:
             url = make_entity_url("person", key, single_page=False)
-            html.append(f"<a href='{esc(url)}' target='person-index' class='item-name'>{esc(name)} ({esc(key)})</a>")
+            html.append(f"<a href='{esc(url)}' target='person-index' class='item-name'>{esc(name)} ({esc(key)}) (reference type: {esc(type)})</a>")
         html.append("</div>")
 
     
     work_ref_names = work.get("work_ref_text")
     work_ref_keys = work.get("work_ref_key")
-    if work_ref_names and work_ref_keys:
+    work_ref_types = work.get("work_ref_type")
+    if work_ref_names and work_ref_keys and work_ref_types:
 
         work_refs = sorted(
-            zip(work_ref_names, work_ref_keys),
+            zip(work_ref_names, work_ref_keys, work_ref_types),
             key=lambda x: normalize_for_sort(x[0])
         )
 
         html.append("<div class='entry-block entry-work-refs'>")
         html.append("<span class='subheading'>Other Works Referenced in Work</span>")
-        for name, key in work_refs:
+        for name, key, type in work_refs:
             url = make_entity_url("work", key, single_page=False)
-            html.append(f"<a href='{esc(url)}' target='work-index' class='item-name'>{esc(name)} ({esc(key)})</a>")
+            html.append(f"<a href='{esc(url)}' target='work-index' class='item-name'>{esc(name)} ({esc(key)}) (reference type: {esc(type)})</a>")
         html.append("</div>")    
 
 
